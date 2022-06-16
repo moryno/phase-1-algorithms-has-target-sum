edeca8404670757756a10c375e3a7c943fbe4bf4
Maurice <mauricenganga41@gmail.com>
2022-06-16 14:58:49 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function hasTargetSum(array, target) {
   for(let i = 0; i < array.length; i++){
-    const complement = target - array[i]
+    
     for(let j = i + 1; j < array.length; j++){
-      if(array[j] === complement) return true;
+      if(array[i] + array[j] === target) return true;
     }
   }
   return false;
